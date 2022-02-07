278c89ff1cf703c05b259bcaf148e33f7e4fc2a4
hayleyskyland <hwitherell@skylandanalytics.net>
2022-02-07 14:32:54 -0700

    Move images to practice templates

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
 const kitties = [
   {
-    KittyName: "Charmmykitty",
-    KittyAge: "8",
-    FavoriteColor: "https://www.provenwinners.com/sites/provenwinners.com/files/imagecache/500x500/ifa_upload/pink_chiffon_hibiscus_0.jpg"
+    name: "Charmmykitty",
+    age: "8",
+    flower: "https://www.provenwinners.com/sites/provenwinners.com/files/imagecache/500x500/ifa_upload/pink_chiffon_hibiscus_0.jpg"
   },
   {
-    KittyName: "Embercat",
-    KittyAge: "2",
-    FavoriteColor: "https://i5.walmartimages.com/asr/555507a6-387b-4972-8207-deaf97fab275_1.11842a3a5487661dea68be2b7f680770.png"
+    name: "Embercat",
+    age: "2",
+    flower: "https://i5.walmartimages.com/asr/555507a6-387b-4972-8207-deaf97fab275_1.11842a3a5487661dea68be2b7f680770.png"
   },
   {
-    KittyName: "Kittay",
-    KittyAge: "13",
-    FavoriteColor: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/green-flowers-green-gerber-daisy-1586803096.jpg"
+    name: "Kittay",
+    age: "13",
+    flower: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/green-flowers-green-gerber-daisy-1586803096.jpg"
   }
 ]
 
@@ -21,17 +21,14 @@ $(function(){
 
       columns: [
         {
-          field: "KittyName",
-          title: "Kitty Name"
+          field: "name",
+          title: "Kitty Name",
+          template: "<div class='name'><img src='#= flower #' />#= name #</div>"
         },
         {
-          field: "KittyAge",
-          title: "Kitty Age"
-        },
-        {
-          field: "FavoriteColor",
-          title: "Favorite Color",
-          template: "<img src='#= FavoriteColor #' />"
+          field: "age",
+          title: "Kitty Age",
+          template: "<div class='age'>#= age #</div>"
         }
       ],
 
