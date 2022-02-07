c7a92b92b20199809f7b19f654e0f391fed0b714
hayleyskyland <hwitherell@skylandanalytics.net>
2022-02-07 16:00:50 -0700

    Add alerts to grid

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 const kitties = [
   {
     name: "Charmmykitty",
-    age: "8",
+    age: "08",
     flower: "https://www.provenwinners.com/sites/provenwinners.com/files/imagecache/500x500/ifa_upload/pink_chiffon_hibiscus_0.jpg"
   },
   {
     name: "Embercat",
-    age: "2",
+    age: "02",
     flower: "https://i5.walmartimages.com/asr/555507a6-387b-4972-8207-deaf97fab275_1.11842a3a5487661dea68be2b7f680770.png"
   },
   {
@@ -23,21 +23,31 @@ $(function(){
         {
           field: "name",
           title: "Kitty Name",
-          template: "<div class='name'><img src='#= flower #' />#= name #</div>"
+          template: "<button id='name'><img src='#= flower #' />#= name #</div>"
         },
         {
           field: "age",
           title: "Kitty Age",
-          template: "<div class='age'>#= age #</div>"
+          template: "<div id='age'>#= age #</div>"
         }
       ],
 
       dataSource: {
         data: kitties
+        // pageSize: 1
       },
 
       width: 400,
-      scrollable: true
+      scrollable: true,
+      // pageable: true,
+      // groupable: true,
+      sortable: true
 
+  });
+})
+
+$(function() {
+  $("button").on("click", function(e) {
+      alert("test");
   });
 })
