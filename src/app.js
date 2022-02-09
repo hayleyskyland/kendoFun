b533474e048a21088448d7be4e812df3e7d10b6f
hayleyskyland <hwitherell@skylandanalytics.net>
2022-02-09 12:26:20 -0700

    Try dynamic index method

--- src/app.js
+++ src/app.js
@@ -10,7 +10,6 @@ const fetchKitties = () => {
   .then(function(data) {
     console.log(data);
     kitties = data.kitties;
-    // console.log(kitties);
   })
 }
 
@@ -23,11 +22,6 @@ const deleteEmberBtn = $("#deleteEmber");
 const deleteKittayBtn = $("#deleteKittay");
 
 //////////// GRID INFO VARIABLES ////////////
-
-// data
-
-// const kitties = [
-// ];
 
 // columns
 
@@ -82,22 +76,22 @@ const show = (elements) => {
 
 // helper - delete individual rows
 
-const deleteRow = (btn, row) => {
-  // let grid = $("#grid").data("kendoGrid");
-  // grid.removeRow(`tr:eq(${row})`);
+const deleteRow = (btn, kitty) => {
   hide([btn]);
-  kitties.splice(row, 1);
+
+  const index = kitties.findIndex(cat => cat.name === kitty);
+
+  kitties.splice(index, 1);
+
   $("#grid").data("kendoGrid").dataSource.data(kitties);
 }
 
 // helper - click individual buttons
 
-const clickKitty = (kitty, btn, row, cookie) => {
-  deleteRow(btn, row);
-  // $.cookie(cookie, "yes");
+const clickKitty = (kitty, btn, cookie) => {
+  deleteRow(btn, kitty);
 
   console.log('deleted:', kitty);
-  // console.log('deletion confirmed:', $.cookie(cookie));
 };
 
 // call all click functions
@@ -108,15 +102,15 @@ $(function() {
   const buttonKittay = $("#Kittay");
 
   buttonCharmmy.on("click", function(e) {
-    clickKitty('Charmmykitty', deleteCharmmyBtn, 0, 'deletedCharmmy');
+    clickKitty('Charmmykitty', deleteCharmmyBtn, 'deletedCharmmy');
   });
 
   buttonEmber.on("click", function(e) {
-    clickKitty('Embercat', deleteEmberBtn, 1, 'deletedEmber');
+    clickKitty('Embercat', deleteEmberBtn, 'deletedEmber');
   });
 
   buttonKittay.on("click", function(e) {
-    clickKitty('Kittay', deleteKittayBtn, 2, 'deletedKittay');
+    clickKitty('Kittay', deleteKittayBtn, 'deletedKittay');
   });
 });
 
@@ -124,33 +118,17 @@ $(function() {
 
 $(function() {
   deleteCharmmyBtn.on("click", function(e) {
-    clickKitty('Charmmykitty', deleteCharmmyBtn, 0, 'deletedCharmmy');
+    clickKitty('Charmmykitty', deleteCharmmyBtn, 'deletedCharmmy');
   });
 
   deleteEmberBtn.on("click", function(e) {
-    clickKitty('Embercat', deleteEmberBtn, 1, 'deletedEmber');
+    clickKitty('Embercat', deleteEmberBtn, 'deletedEmber');
   });
 
   deleteKittayBtn.on("click", function(e) {
-    clickKitty('Kittay', deleteKittayBtn, 2, 'deletedKittay');
+    clickKitty('Kittay', deleteKittayBtn, 'deletedKittay');
   });
 });
-
-// call cookies
-
-// $(function() {
-//   if ($.cookie("deletedCharmmy") === "yes") {
-//     deleteRow(deleteCharmmyBtn, 0);
-//   };
-
-//   if ($.cookie("deletedEmber") === "yes") {
-//     deleteRow(deleteEmberBtn, 1);
-//   };
-
-//   if ($.cookie("deletedKittay") === "yes") {
-//     deleteRow(deleteKittayBtn, 2);
-//   };
-// });
 
 //////////// UNDO BUTTON ////////////
 
@@ -158,10 +136,6 @@ $(function() {
   const undoBtn = $("#undoBtn");
 
   undoBtn.on("click", function(e) {
-    // $.cookie("deletedCharmmy", "no", { "expires": 7 });
-    // $.cookie("deletedEmber", "no", { "expires": 7 });
-    // $.cookie("deletedKittay", "no", { "expires": 7 });
-
     fetchKitties();
 
     show([deleteCharmmyBtn, deleteEmberBtn, deleteKittayBtn]);
